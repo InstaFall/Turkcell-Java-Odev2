0072b1e8389c9a683cb0cd3ccbe22e49d0b67fb7
Can Akilli <gorkemcanakilli@hotmail.com>
2024-03-01 02:47:06 +0300

    Fix usage of correct overloaded function

--- src/main/java/com/turkcelljava/odev2/controllers/LanguagesController.java
+++ src/main/java/com/turkcelljava/odev2/controllers/LanguagesController.java
@@ -54,7 +54,7 @@ public class LanguagesController {
     //Updating language
     @PutMapping("{id}")
     public String putLanguage(@RequestBody Language language, @PathVariable("id") int id) {
-        if (languageService.updateLanguage(id, language)) {
+        if (languageService.updateLanguage(id, language.getName())) {
             return "Updated language with id " + id;
         } else return "Could not update the language!";
     }
